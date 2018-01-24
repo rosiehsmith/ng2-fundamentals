575e3bb99342f16f5e9a3df68f8bfaac6f219dd7
rosiehighsmith <rosie.highsmith@gmail.com>
2018-01-24 13:42:00 -0800

    communicate between components

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
     <div>
         <h1>Upcoming Angular 2 Events</h1>
         <hr>
-        <event-thumbnail></event-thumbnail>
+        <event-thumbnail [event]="event1"></event-thumbnail>
     </div>      
     ` 
 })
 
 export class EventsListComponent{
-    event = {
+    event1 = {
         id: 1,
         name: 'Angular Connect',
         date: '9/26/2036',
